8c48de926c25793a3c14f42ed1179854f5a1cbd6
20Koen02 <mail@koen02.nl>
2021-05-03 11:25:41 +0200

    f13 - f17

--- keyboards/handwired/void9/keymaps/default/keymap.c
+++ keyboards/handwired/void9/keymaps/default/keymap.c
@@ -19,48 +19,48 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_ortho_3x3(
-        KC_MPRV,         KC_MNXT,         RESET,
-        CU_10,           CU_11,           KC_MPLY,
-        CU_20,           CU_21,           CU_22
+        KC_MPRV,          KC_MNXT,          RESET,
+        KC_F13,           KC_F14,           KC_MPLY,
+        KC_F15,           KC_F16,           KC_F17
     )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-        case CU_00 ... CU_01: // Skip
+    // switch (keycode) {
+    //     case CU_00 ... CU_01: // Skip
 
-        case CU_10:
-            if (record->event.pressed) {
-                SEND_STRING("Macro!");
-            }
-            break;
-        
-        case CU_11:
-            if (record->event.pressed) {
-                SEND_STRING("Macro!");
-            }
-            break;
-    
-        case CU_12: // Skip
+    //     case CU_10:
+    //         if (record->event.pressed) {
+    //             SEND_STRING("Macro!");
+    //         }
+    //         break;
 
-        case CU_20:
-            if (record->event.pressed) {
-                SEND_STRING("Macro!");
-            }
-            break;
-        
-        case CU_21:
-            if (record->event.pressed) {
-                SEND_STRING("Macro!");
-            }
-            break;
+    //     case CU_11:
+    //         if (record->event.pressed) {
+    //             SEND_STRING("Macro!");
+    //         }
+    //         break;
 
-        case CU_22:
-            if (record->event.pressed) {
-                SEND_STRING("Macro!");
-            }
-            break;
-    }
+    //     case CU_12: // Skip
+
+    //     case CU_20:
+    //         if (record->event.pressed) {
+    //             SEND_STRING("Macro!");
+    //         }
+    //         break;
+
+    //     case CU_21:
+    //         if (record->event.pressed) {
+    //             SEND_STRING("Macro!");
+    //         }
+    //         break;
+
+    //     case CU_22:
+    //         if (record->event.pressed) {
+    //             SEND_STRING("Macro!");
+    //         }
+    //         break;
+    // }
     return true;
 };
 
